4206f8905d59e6deafa1e69de0bbc6686f6ed129
zukelwa20 <makhosandilzukelwa@gmail.com>
2016-11-18 09:55:56 +0200

    modified fromWhere.js

--- fromWhere.js
+++ fromWhere.js
@@ -17,12 +17,12 @@ var fromWhere = function(carReg){
   return "Some other place!";
     }
   };
-assert.equal(fromWhere('CY 123456'),'Bellville');
-assert.equal(fromWhere('CJ 456987'),'paarl');
-assert.equal(fromWhere('CA 789654'),'capetown');
-assert.equal(fromWhere('GP 741852'),'Some other place!');
+//assert.equal(fromWhere('CY 123456'),'Bellville');
+//assert.equal(fromWhere('CJ 456987'),'paarl');
+//assert.equal(fromWhere('CA 789654'),'capetown');
+//assert.equal(fromWhere('GP 741852'),'Some other place!');
 
 //console.log(fromWhere('CY 123456'));
 //console.log(fromWhere('CJ 456987'));
 //console.log(fromWhere('CA 789654'));
-//console.log(fromWhere('some other place!'));
+//console.log(fromWhere('GP 123654'));
